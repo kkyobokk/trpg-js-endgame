3615d6784e1ff23c14817f9cfa45a924ac28a412
송은빈 <trpgtravel@gmail.com>
2024-02-03 03:13:55 +0900

    third step

--- src/src/Component/StaTable.js
+++ src/src/Component/StaTable.js
@@ -1,4 +1,6 @@
 export default function StaTable({name,stat}) {
     const StatName = {"MaxHP":"체력","ATK":"힘","LUK":"행운","MAG":"마력","ADEF":"물리 방어","MDEF":"마법 방어","MaxMANA":"마나"}
-    return StatName[name] !== undefined ? <div><h1>{StatName[name]} : {stat}</h1></div> : null
+    return <div className="StaTable">
+            <h1 style={{float:"left",paddingRight:"100px"}}>{StatName[name]} : {stat}</h1>
+        </div>
 }
--- src/src/Component/Stat.js
+++ src/src/Component/Stat.js
@@ -7,15 +7,33 @@ import StaTable from './StaTable'
 export default function Stat() {
     const [StatData, setStatData] = useState(PlayerData.Stat)
 
-    const ATKup = function() {
+    const Statup = function(name) {
+        let res = (name) => {switch (name){
+            case "MaxHP":
+                return {...StatData,MaxHP : StatData.MaxHP + 5} 
+            case "ATK":
+                return {...StatData,ATK : StatData.ATK + 1}
+            case "LUK":
+                return {...StatData,LUK : StatData.LUK + 1}
+            case "MAG":
+                return {...StatData,MAG : StatData.MAG + 1}
+            case "ADEF":
+                return {...StatData,ADEF : StatData.ADEF + 1}
+            case "MDEF":
+                return {...StatData,MDEF : StatData.MDEF + 1}
+            case "MaxMANA":
+                return {...StatData,MaxMANA : StatData.MaxMANA + 5}
+            default:
+                return 0
+            }
+        }
         setStatData(() => {
-            const res = {...StatData,ATK : StatData.ATK + 1}
             //ipcRenderer.send('Stat',{...PlayerData, Stat : res})
-            return res
+            return res(name)
         })
     }
 
     return <div className="center">
-        {[...Object.keys(PlayerData.Stat)].map(e => <StaTable name={e} stat={PlayerData.Stat[e]}></StaTable>)}
+        {[...Object.keys(PlayerData.Stat)].map(e => e !== "CurHP" ? e !== "CurMANA" ? <div><StaTable name={e} stat={StatData[e]}></StaTable><button onClick={() => Statup(e)} className='StatUpBtn'>스탯 업</button></div> : null : null)}
     </div>
 }
